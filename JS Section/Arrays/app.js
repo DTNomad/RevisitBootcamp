ddeef3a4267ee036012c237b4600cf8533f30eed
DTNomad <84250615+DTNomad@users.noreply.github.com>
2021-06-25 23:29:22 -0700

    Update app.js with arr manipulation

--- JS Section/Arrays/app.js
+++ JS Section/Arrays/app.js
@@ -1,3 +1,10 @@
 const leaderboard = ['Harry', 'Lua', 'Hermione', 'Bellatrix'];
 
 leaderboard[1] = 'Luna';
+
+leaderboard.push('Damon');
+leaderboard.pop('Harry');
+let first = leaderboard.shift();
+console.log(first);
+leaderboard.unshift('Hairy');
+console.log(leaderboard);
